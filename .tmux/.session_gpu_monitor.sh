643a0e01c92b1d31ccdcc9822aca1a0eefd4559a
williamgrimes <will-grimes@gmx.com>
2018-02-27 19:27:02 +0000

    update symlink script

--- .tmux/.session_gpu_monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-SESSION_NAME="gpu"
-tmux new-session -s $SESSION_NAME -d htop
-tmux split-window -v glances
-tmux select-pane -t 1
-tmux rename-window "monitor"
-tmux attach-session -t $SESSION_NAME
