5ce873a4768e5bfe8b9c0357530377acc420d048
williamgrimes <will-grimes@gmx.com>
2018-02-27 19:33:51 +0000

    updated symlinks

--- symlink_setup_local.sh
+++ symlink_setup_local.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ln -s ~/.dotfiles/.aliasrc ~/.aliasrc
-ln -s ~/.dotfiles/.alias_ssh ~/.alias_ssh
-ln -s ~/.dotfiles/.bash_functions ~/.bash_functions
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.pythonstartup ~/.pythonstartup
