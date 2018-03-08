7514db63cd2cddfdee6975397dd8a69165a382cb
williamgrimes <will-grimes@gmx.com>
2018-03-08 12:49:23 +0000

    added symlink for psql

--- symlink_setup_remote.sh
+++ symlink_setup_remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -s ~/.dotfiles/.aliasrc.remote ~/.aliasrc
 ln -s ~/.dotfiles/.bashrc.remote ~/.bashrc
+ln -s ~/.dotfiles/.psqlrc ~/.psqlrc
 ln -s ~/.dotfiles/.tmux.conf.remote ~/.tmux.conf
 ln -s ~/.dotfiles/.vimrc.remote ~/.vimrc
-
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
