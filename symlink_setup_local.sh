3d0543147074af37855c9a5274347cb5ebc77274
williamgrimes <will-grimes@gmx.com>
2018-03-11 16:18:29 +0000

    added xmodmap for hjkl global navigation

--- symlink_setup_local.sh
+++ symlink_setup_local.sh
@@ -4,5 +4,6 @@ ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.pythonstartup ~/.pythonstartup
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.xmodmap ~/.xmodmap
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
